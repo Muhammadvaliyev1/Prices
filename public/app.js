720513181434d24415cdd4da80cbd0dc2d61320c
Muhammadvaliyev1 <127742778+Muhammadvaliyev1@users.noreply.github.com>
2024-01-31 17:11:31 +0500

    add new products

--- public/app.js
+++ public/app.js
@@ -1,24 +1,24 @@
 window.onload = () => {
-  let input = document.querySelector('#input')
-  input.oninput = function(){
-    let value = this.value.trim();
-    let list = document.querySelectorAll('#clas')
-    
+  let input = document.querySelector('#input');
+  input.oninput = function () {
+    let value = this.value.trim().toLowerCase(); // Convert input value to lowercase
+    let list = document.querySelectorAll('#clas');
 
-    if(value != '') {
-
+    if (value !== '') {
       list.forEach(elem => {
-        if(elem.innerText.search(value) == -1) {
-          elem.classList.add('hidden')
+        // Convert inner text to lowercase and then check for a match
+        if (elem.innerText.toLowerCase().search(value) === -1) {
+          elem.classList.add('hidden');
+        } else {
+          elem.classList.remove('hidden');
         }
-      })
+      });
     } else {
       list.forEach(elem => {
-        elem.classList.remove('hidden')
-      })
+        elem.classList.remove('hidden');
+      });
     }
-    
 
     console.log(this.value);
-  }
-}
+  };
+};
